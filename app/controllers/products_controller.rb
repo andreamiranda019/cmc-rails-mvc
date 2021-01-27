70ce2be334646f1c917228bdf1b77a260cec33d5
chokolove <carlosll2493@gmail.com>
2021-01-27 17:39:14 -0500

    Add ProductController methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,18 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[show edit update destroy] 
+
   def new
+    @product = Product.new()
   end
 
   def create
+    category = Category.find(params[:category_id])
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new
+    end
   end
 
   def show
@@ -12,8 +22,26 @@ class ProductsController < ApplicationController
   end
 
   def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @product.destroy
+
+    redirect_to root_path
   end
+
+  private
+    def set_product
+      @product =  Product.find(params[:id]) 
+    end
+    
+    def product_params
+      params.require(:product).permit(:name, :category_id, :code,
+         :price, :brand, :material, :type, :color, :size, :use)
+    end
 end
