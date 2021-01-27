d10fafb580b3aa363c0b30673c7c4d8382800ec9
chokolove <carlosll2493@gmail.com>
2021-01-27 17:08:23 -0500

    Generate Product controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :categories only: %i[index show]do
+    resources :product, only: %i[show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
